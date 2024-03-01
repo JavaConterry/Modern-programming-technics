cb72315f4ead3a3e7a3dc311553b50c3d722d1f2
JavaConterry <conterry05@gmail.com>
2024-03-01 23:03:24 +0100

    test

--- dumb_code.py
+++ dumb_code.py
@@ -1,5 +1,7 @@
 # it was said to commit a DUMB code
 
+import random
+
 class Wheel:
     wheel_diameter = 85
     aspect_ratio = 65
@@ -23,17 +25,34 @@ class Wheel:
 
 
 class Car(Wheel):
-    model_id = 'XXXXXXXXX'
-    details = {'Manufacturer': 'XXXXXX', 'Model': 'xxxxxxx'}
+    # model_id
+    # details
+
+    def generate_model_id(self):
+        self.model_id = random.randint(1000, 9999) 
+
+    def generate_dummy_details(self):
+        self.details = {'Manufacturer': 'Unknown', 'Model': 'Unknown'}
 
     def __init__(self, model_id, details, wheel_diameter, aspect_ratio, construction_type, load_index):
-        self.model_id = model_id
-        self.details = details
+        if(model_id is None):
+            self.generate_model_id()
+        else:
+            self.model_id = model_id
+        if(details is None):
+            self.generate_dummy_details()
+        else:
+            self.details = details
+        
 
         Wheel.__init__(self, wheel_diameter, aspect_ratio, construction_type, load_index)
 
     def __str__(self):
         return f"model_id: {self.model_id}, details: {self.details}, {super().__str__()}"
+
+
+
+
 
 car = Car('1234', {'Manufacturer': 'Porsche', 'Model': '911 whats your emergency?'}, 225, 55, 'R', 'V')
 
--- tests.py
+++ tests.py
@@ -7,5 +7,13 @@ class TestClass(unittest.TestCase):
         dumb_car.rotate(90)
         self.assertEqual(dumb_car.wheel_angle, 90, 'Your car is dumb in other ways')
 
+    def test_details_generating(self):
+        dumb_car = Car(None, None, 225, 55, 'R', 'V')
+        id_count_of_digits = len(str(dumb_car.model_id))
+        self.assertEqual(id_count_of_digits, 4)
+        da = 'da' if dumb_car.details is not None else 'ne'
+        self.assertEqual(da, 'da')
+
+        
 if __name__ == '__main__':
     unittest.main()
